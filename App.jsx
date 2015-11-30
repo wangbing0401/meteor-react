fc31923402de1f9234c79b94f7cf4e5989a124c8
wangbing <wangb@tongxinyiliao.com>
2015-11-30 20:32:13 +0800

    meteor-react

--- App.jsx
+++ App.jsx
@@ -1,4 +1,13 @@
 App = React.createClass({
+    // This mixin makes the getMeteorData method work
+    mixins: [ReactMeteorData],
+
+    // Loads items from the Tasks collection and puts them on this.data.tasks
+    getMeteorData() {
+        return {
+            tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+        }
+    },
     getTasks() {
         return [
             { _id: 1, text: "This is task 1" },
@@ -8,9 +17,23 @@ App = React.createClass({
     },
 
     renderTasks() {
-        return this.getTasks().map((task) => {
-            return <Task key={task._id} task={task.text} />;
+        return this.data.tasks.map((task) => {
+            return <Task key={task._id} task={task} />;
         });
+    },
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        Tasks.insert({
+            text: text,
+            createdAt: new Date() // current time
+        });
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = "";
     },
 
     render() {
@@ -18,6 +41,12 @@ App = React.createClass({
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    <form className="new-task" onSubmit={this.handleSubmit} >
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Type to add new tasks" />
+                    </form>
                 </header>
 
                 <ul>
