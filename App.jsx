d75a367e1165a2182c227f946851c9d9a4bd67cf
wangbing <wangb@tongxinyiliao.com>
2015-11-30 21:26:53 +0800

    去除insecure包

--- App.jsx
+++ App.jsx
@@ -41,12 +41,7 @@ App = React.createClass({
 
         // Find the text field via the React ref
         var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-        Tasks.insert({
-            text: text,
-            createdAt: new Date(), // current time
-            owner: Meteor.userId(),           // _id of logged in user
-            username: Meteor.user().username  // username of logged in user
-        });
+        Meteor.call("addTask", text);
 
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = "";
--- meteor-react.jsx
+++ meteor-react.jsx
@@ -13,3 +13,29 @@ if (Meteor.isClient) {
         ReactDOM.render(<App />, document.getElementById("render-target"));
     });
 }
+
+if (Meteor.isServer){
+    Meteor.methods({
+        addTask(text) {
+            // Make sure the user is logged in before inserting a task
+            if (! Meteor.userId()) {
+                throw new Meteor.Error("not-authorized");
+            }
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date(),
+                owner: Meteor.userId(),
+                username: Meteor.user().username
+            });
+        },
+
+        removeTask(taskId) {
+            Tasks.remove(taskId);
+        },
+
+        setChecked(taskId, setChecked) {
+            Tasks.update(taskId, { $set: { checked: setChecked} });
+        }
+    });
+}
