5531fe4a86cd65be343e3a772b45d9bfbbfeab85
wangbing <wangb@tongxinyiliao.com>
2016-01-18 16:41:57 +0800

    loading层bug

--- client/template/atom/Atom_detail.jsx
+++ client/template/atom/Atom_detail.jsx
@@ -16,14 +16,13 @@ AtomDetail = React.createClass({
     componentWillMount:function(){
         var self = this;
         this.getData(function(result){
-            console.log(result);
             self.setState({data:result});
         });
     },
     render: function(){
         return (
             <div>
-                <h5 className="atom_detail_title">{this.state.data.title}</h5>
+                <h3 className="atom_detail_title">{this.state.data.title}</h3>
                 <div className="atom_detail_author">作者：{this.state.data.user && this.state.data.user.username}</div>
                 <div style={{clear:'both'}}></div>
                 <div className="atom_detail_content">{this.state.data.content}</div>
