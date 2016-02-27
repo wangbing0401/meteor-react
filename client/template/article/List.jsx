0c006238547216d03cfc164b69d826f354a9c90f
wangbing <wangb@tongxinyiliao.com>
2016-02-27 17:08:11 +0800

    完成文章和帖子滚动刷新功能

--- client/template/article/List.jsx
+++ client/template/article/List.jsx
@@ -1,33 +1,44 @@
+var article_count;
 List = React.createClass({
     mixins: [ReactMeteorData],
-    getMeteorData: function(){
+    getMeteorData: function(mark){
+        var mark = mark || 1;
+        Meteor.subscribe('article_count');
         PubSub.publish('loading_show', true);
         var data = {};
-        var handle = Meteor.subscribe("get_article_list");
+        var handle = Meteor.subscribe("get_article_list", mark);
         if(handle.ready()){
             PubSub.publish('loading_show', false);
             data.articles = Article.find({}, {sort:{create_time:-1}}).fetch();
+            article_count = data.articles.length;
         }
         return data;
     },
     componentDidMount:function(){
-        var waypoint = new Waypoint({
-            element: document.getElementsByClassName('article_last'),
-            handler: function(direction) {
-                console.log('Handler triggered in ' + direction + ' direction');
-            },
-            context: document.getElementsByClassName('article_content')
+        var self = this;
+        var mark = 1;
+        $(window).scroll(function(){
+            if($(document).scrollTop()>=$(document).height()-$(window).height()){
+                if(Counts.get('article_count') != article_count){
+                    self.getMeteorData(++mark);
+                }else{
+                    WB.dialog_show("没有更多数据了");
+                }
+            }
         });
+    },
+    componentWillUnmount: function(){
+        $(window).unbind("scroll");
     },
     render:function(){
         return (
-            <div className="article_content" style={{height:'300px', overflow:'scroll'}}>
+            <div className="article_content" style={{overflow:'scroll'}}>
                 {
                     this.data.articles?this.data.articles.map((data) => {
                         return <Item key={data._id} articleId={data._id} title={data.title} content={data.content} image_url={data.imageUrl} />
                     }):''
                 }
-                <div style={{height:'500px'}} className="article_last"></div>
+                <div style={{height:'5px'}} className="article_last"></div>
             </div>
         );
     }
--- client/template/atom/Atom.jsx
+++ client/template/atom/Atom.jsx
@@ -1,14 +1,34 @@
+var atom_count;
 Atom = React.createClass({
     mixins:[ReactMeteorData],
-    getMeteorData:function(){
+    getMeteorData:function(mark){
+        Meteor.subscribe('atom_count');
+        var mark = mark || 1;
         PubSub.publish('loading_show', true);
         var data = {};
-        var handle = Meteor.subscribe("get_post_list");
+        var handle = Meteor.subscribe("get_post_list", mark);
         if(handle.ready()){
             PubSub.publish('loading_show', false);
             data.posts = Posts.find({}, {sort:{create_time:-1}}).fetch();
+            atom_count = data.posts.length;
         }
         return data;
+    },
+    componentDidMount: function(){
+        var self = this;
+        var mark = 1;
+        $(window).scroll(function(){
+            if($(document).scrollTop()>=$(document).height()-$(window).height()){
+                if(Counts.get('atom_count') != atom_count){
+                    self.getMeteorData(++mark);
+                }else{
+                    WB.dialog_show("没有更多数据了");
+                }
+            }
+        });
+    },
+    componentWillUnmount: function(){
+        $(window).unbind("scroll");
     },
     publish_atom:function(){
         FlowRouter.go('publish_atom');
